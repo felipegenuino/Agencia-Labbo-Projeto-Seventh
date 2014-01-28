1fe1f9eb9d04470aadb7b436056cbfa9808d46b0
felipegenuino <design@felipegenuino.com>
2014-01-28 02:06:53 -0200

    Update commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 //Plugin Carousel
 $(document).ready(function() {
      $("#slider-main").owlCarousel({
-        navigation : false, // Show next and prev buttons
+        navigation : true, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
         singleItem:true,
